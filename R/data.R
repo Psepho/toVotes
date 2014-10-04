78dab8b014ecf00570da633396a58d674faeefc8
Matthew Routley <matt@routleynet.org>
2014-10-04 13:34:08 -0400

    Add in geocoded data

--- R/data.R
+++ R/data.R
@@ -16,3 +16,26 @@
 #' @source \url{http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=834689fe9c18b210VgnVCM1000003dd60f89RCRD}
 #' @name toVotes
 NULL
+#' Geographic data for the polling stations in Toronto municipal elections.
+#'
+#' Geocoded poll locations from the 2003, 2006, and
+#' 2010 municipal elections in Toronto.
+#'
+#' \itemize{
+#'   \item long. The longitude of the polling station
+#'   \item lat. The latitude of the polling station
+#'   \item order. Geocoding of the polling station
+#'   \item hole. Geocoding of the polling station
+#'   \item piece. Geocoding of the polling station
+#'   \item group. Geocoding of the polling station
+#'   \item id. A character string with two characters for ward and three for area
+#'   \item year. The year in which the polling station was used
+#'   \item ward. The ward of the polling station
+#'   \item area. The area of the polling station
+#'   \item ward name. The name of the ward containing the polling station
+#'   ...
+#' }
+#'
+#' @format A data frame with 387422 rows and 12 variables
+#' @name toPollGeo
+NULL
---
+++ R/geocode.R
@@ -0,0 +1,46 @@
+library(ggmap)
+library(rgeos)
+library(maptools)
+library(stringr)
+library(dplyr)
+
+# Download shapefiles -----------------------------------------------------
+
+years <- c(2006,2010)
+for (year in years) {
+  if(file.exists(paste("tmp/subdivisions_",year,".zip",sep=""))) {
+    # Nothing to do
+  }  else {
+    download.file(paste("http://opendata.toronto.ca/gcc/voting_subdivision_",year,"_wgs84.zip",sep=""),
+                  destfile = paste("tmp/subdivisions_",year,".zip",sep=""))
+    unzip(paste("tmp/subdivisions_",year,".zip",sep=""), exdir="tmp")
+  }
+  shape <- paste("shapefile_",year,sep="")
+  file <- paste("tmp/VOTING_SUBDIVISION_",year,"_WGS84.shp",sep="")
+  assign(shape,readShapeSpatial(file, proj4string=CRS("+proj=longlat +datum=WGS84")))
+  rm(file,shape)
+}
+rm(year,years)
+
+# Shapefiles --------------------------------------------------------------
+
+# Extract the data object for each year and then combine into one data frame
+geo_2010 <- fortify(shapefile_2010,region="AREA_NAME")
+geo_2010$year <- as.integer(2010)
+geo_2006 <- fortify(shapefile_2006,region="AREA_NAME")
+geo_2006$year <- as.integer(2006)
+geo_2003 <- fortify(shapefile_2006,region="AREA_NAME") # Assuming 2006 locations for 2003 geo
+geo_2003$year <- as.integer(2003)
+toPollGeo <- rbind(geo_2010,geo_2006,geo_2003)
+toPollGeo$ward <- as.integer(str_sub(toPollGeo$id,1,2))
+toPollGeo$area <- as.integer(str_sub(toPollGeo$id,-3,-1))
+# For 2014, assume same geography as 2010
+geo_2014 <- toPollGeo %.%
+  filter(year==2010) %.%
+  mutate(year=2014)
+toPollGeo <- rbind(toPollGeo, geo_2014)
+toPollGeo$year <- as.factor(toPollGeo$year)
+rm(geo_2014, geo_2003, geo_2006, geo_2010)
+ward_regions <- read.csv("data/ward_regions.csv")
+toPollGeo <- inner_join(toPollGeo,ward_regions, by=c("ward"))
+rm(ward_regions)
