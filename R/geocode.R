384e9962000c3e91a93bb3a31554e6f4f4b8e85a
Matthew Routley <matt@routleynet.org>
2014-10-04 16:16:47 -0400

    Tidy up shapefile downloads

--- R/geocode.R
+++ R/geocode.R
@@ -1,46 +1,43 @@
-library(ggmap)
-library(rgeos)
-library(maptools)
-library(stringr)
-library(dplyr)
+geocodePolls <- function() {
+  # Download shapefiles -----------------------------------------------------
 
-# Download shapefiles -----------------------------------------------------
-
-years <- c(2006,2010)
-for (year in years) {
-  if(file.exists(paste("tmp/subdivisions_",year,".zip",sep=""))) {
-    # Nothing to do
-  }  else {
-    download.file(paste("http://opendata.toronto.ca/gcc/voting_subdivision_",year,"_wgs84.zip",sep=""),
-                  destfile = paste("tmp/subdivisions_",year,".zip",sep=""))
-    unzip(paste("tmp/subdivisions_",year,".zip",sep=""), exdir="tmp")
+  years <- c(2006,2010)
+  for (year in years) {
+    if(file.exists(paste("subdivisions_",year,".zip",sep=""))) {
+      # Nothing to do
+    }  else {
+      download.file(paste("http://opendata.toronto.ca/gcc/voting_subdivision_",year,"_wgs84.zip",sep=""),
+                    destfile = paste("subdivisions_",year,".zip",sep=""))
+      unzip(paste("subdivisions_",year,".zip",sep=""), exdir="")
+    }
+    shape <- paste("shapefile_",year,sep="")
+    file <- paste("VOTING_SUBDIVISION_",year,"_WGS84.shp",sep="")
+    assign(shape,maptools::readShapeSpatial(file, proj4string=CRS("+proj=longlat +datum=WGS84")))
+    rm(file,shape)
   }
-  shape <- paste("shapefile_",year,sep="")
-  file <- paste("tmp/VOTING_SUBDIVISION_",year,"_WGS84.shp",sep="")
-  assign(shape,readShapeSpatial(file, proj4string=CRS("+proj=longlat +datum=WGS84")))
-  rm(file,shape)
+  rm(year,years)
+
+  # Shapefiles --------------------------------------------------------------
+
+  # Extract the data object for each year and then combine into one data frame
+  geo_2010 <- ggplot2::fortify(shapefile_2010,region="AREA_NAME")
+  geo_2010$year <- as.integer(2010)
+  geo_2006 <- ggplot2::fortify(shapefile_2006,region="AREA_NAME")
+  geo_2006$year <- as.integer(2006)
+  geo_2003 <- ggplot2::fortify(shapefile_2006,region="AREA_NAME") # Assuming 2006 locations for 2003 geo
+  geo_2003$year <- as.integer(2003)
+  toPollGeo <- rbind(geo_2010,geo_2006,geo_2003)
+  toPollGeo$ward <- as.integer(stringr::str_sub(toPollGeo$id,1,2))
+  toPollGeo$area <- as.integer(stringr::str_sub(toPollGeo$id,-3,-1))
+  # For 2014, assume same geography as 2010
+  geo_2014 <- toPollGeo %.%
+    dplyr::filter(year==2010) %.%
+    dplyr::mutate(year=2014)
+  toPollGeo <- rbind(toPollGeo, geo_2014)
+  toPollGeo$year <- as.factor(toPollGeo$year)
+  rm(geo_2014, geo_2003, geo_2006, geo_2010)
+  ward_regions <- read.csv("data/ward_regions.csv")
+  toPollGeo <- dplyr::inner_join(toPollGeo,ward_regions, by=c("ward"))
+  rm(ward_regions)
+  toPollGeo
 }
-rm(year,years)
-
-# Shapefiles --------------------------------------------------------------
-
-# Extract the data object for each year and then combine into one data frame
-geo_2010 <- fortify(shapefile_2010,region="AREA_NAME")
-geo_2010$year <- as.integer(2010)
-geo_2006 <- fortify(shapefile_2006,region="AREA_NAME")
-geo_2006$year <- as.integer(2006)
-geo_2003 <- fortify(shapefile_2006,region="AREA_NAME") # Assuming 2006 locations for 2003 geo
-geo_2003$year <- as.integer(2003)
-toPollGeo <- rbind(geo_2010,geo_2006,geo_2003)
-toPollGeo$ward <- as.integer(str_sub(toPollGeo$id,1,2))
-toPollGeo$area <- as.integer(str_sub(toPollGeo$id,-3,-1))
-# For 2014, assume same geography as 2010
-geo_2014 <- toPollGeo %.%
-  filter(year==2010) %.%
-  mutate(year=2014)
-toPollGeo <- rbind(toPollGeo, geo_2014)
-toPollGeo$year <- as.factor(toPollGeo$year)
-rm(geo_2014, geo_2003, geo_2006, geo_2010)
-ward_regions <- read.csv("data/ward_regions.csv")
-toPollGeo <- inner_join(toPollGeo,ward_regions, by=c("ward"))
-rm(ward_regions)
