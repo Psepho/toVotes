ac0847c54826e802ccfc9a07a14bbab07041c10a
Matthew Routley <matt@routleynet.org>
2015-03-18 16:44:24 -0400

    Add 2008 federal results

--- data-raw/federal_results.R
+++ data-raw/federal_results.R
@@ -1,8 +1,11 @@
+# 2008 http://www.elections.ca/scripts/OVR2008/31/data/pollresults_resultatsbureau_canada.zip
+# 2011 http://elections.ca/scripts/OVR2011/34/data_donnees/pollresults_resultatsbureau_canada.zip
+
 zip_file <- "data-raw/pollresults_resultatsbureau_canada.zip"
 if(file.exists(zip_file)) { # Only download the data once
   # File exists, so nothing to do
 }  else {
-  download.file("http://elections.ca/scripts/OVR2011/34/data_donnees/pollresults_resultatsbureau_canada.zip",
+  download.file("http://www.elections.ca/scripts/OVR2008/31/data/pollresults_resultatsbureau_canada.zip",
                 destfile = zip_file)
   unzip(zip_file, exdir=paste("data-raw", "pollresults", sep = "/")) # Extract the data into data-raw
 }
@@ -13,9 +16,9 @@ on_files <- list.files(path = "data-raw/pollresults",
 federal_results <- do.call("rbind", lapply(on_files, function(.file){dplyr::tbl_df(read.csv(.file, na.strings = c("NA")))}))
 federal_results <- dplyr::transmute(federal_results,
                             candidate = as.factor(paste(Candidate.s.Family.Name.Nom.de.famille.du.candidat, Candidate.s.First.Name.Prénom.du.candidat, sep = " ")),
-                            year = as.factor(2011),
+                            year = as.factor(2008),
                             type = "federal",
-                            votes = Candidate.Poll.Votes.Count.Votes.du.candidat.pour.le.bureau,
+                            votes = as.integer(Candidate.Poll.Votes.Count.Votes.du.candidat.pour.le.bureau),
                             party = Political.Affiliation.Name_English.Appartenance.politique_Anglais,
                             district = as.character(Electoral.District.Number.Numéro.de.circonscription),
                             poll = as.character(Polling.Station.Number.Numéro.du.bureau.de.scrutin),
@@ -23,14 +26,18 @@ federal_results <- dplyr::transmute(federal_results,
                             )
 to_ed <- read.csv("data-raw/TO_federal_election_districts.csv")[,1]
 to_ed <- data.frame(district = as.character(to_ed[!is.na(to_ed)]))
-toFederalVotes <- dplyr::inner_join(federal_results, to_ed)
+to_votes <- dplyr::inner_join(federal_results, to_ed)
+toFederalVotes <- rbind(toFederalVotes, to_votes)
+
+toFederalVotes$votes <- as.integer(toFederalVotes$votes)
 
 save(toFederalVotes, file = "data/toFederalVotes.rda")
 
 # Check Beaches results
-to_federal_results %>%
+library(dplyr)
+toFederalVotes %>%
   filter(district == "35005") %>%
-  select(candidate, votes) %>%
-  group_by(candidate) %>%
+  select(candidate, year, votes) %>%
+  group_by(candidate, year) %>%
   summarize(votes = sum(votes))
 
