778fc960eb9b6aec802b8283893ef0ff9587cee0
Matthew Routley <matt@routleynet.org>
2018-09-05 15:14:43 -0400

    Switch to sf package, which simplifies things

--- R/geocode.R
+++ R/geocode.R
@@ -1,35 +1,38 @@
 geocodePolls <- function() {
   # Download shapefiles -----------------------------------------------------
   years <- c(2006, 2010, 2014)
+  base_shapefile_url <- "http://opendata.toronto.ca/gcc/"
+  shapefile_prefix <- "data-raw/subdivision_"
   for (year in years) {
-    if(file.exists(paste("data-raw/subdivisions_",year,".zip",sep=""))) {
+    if(file.exists(paste(shapefile_prefix, year,".zip",sep=""))) {
       # Nothing to do
     }  else {
-      download.file(paste("http://opendata.toronto.ca/gcc/", ifelse(year == 2014,"voting_subdivsion_", "voting_subdivision_"), year, "_wgs84.zip", sep = ""),
-                    destfile = paste("data-raw/subdivisions_",year,".zip",sep=""))
-      unzip(paste("data-raw/subdivisions_", year, ".zip", sep=""), exdir="data-raw")
+      download.file(paste0(base_shapefile_url, ifelse(year == 2014, "voting_subdivsion_", "voting_subdivision_"), year, "_wgs84.zip"),
+                    destfile = paste0(shapefile_prefix,year,".zip"))
+      unzip(paste(shapefile_prefix, year, ".zip", sep=""), exdir=paste0("data-raw/", year))
     }
-    shape <- paste("shapefile_", year, sep="")
-    file <- paste("VOTING_SUBDIVISION_",year,"_WGS84",sep="")
-    assign(shape,rgdal::readOGR(dsn = "data-raw", layer = file))
-    rm(file,shape)
+
+    assign(paste("geo", year, sep = "_"),
+           sf::st_read(paste0("data-raw/", year)) %>%
+             sf::st_transform(crs = "+init=epsg:4326") %>% # Not strictly necessary, since already using this projection
+             dplyr::mutate(ward = as.integer(stringr::str_sub(AREA_LONG, 1, 2)), # Extract Ward ID
+                           area = as.integer(stringr::str_sub(AREA_LONG, -3, -1)), # Extract Area ID
+                           year = as.factor(year)) %>%
+             dplyr::select(ward, area, year, geometry)
+    )
   }
-  rm(year,years)
-  # Shapefiles --------------------------------------------------------------
-  # Extract the data object for each year and then combine into one data frame
-  geo_2014 <- ggplot2::fortify(shapefile_2014,region="AREA_NAME")
-  geo_2014$year <- as.integer(2014)
-  geo_2010 <- ggplot2::fortify(shapefile_2010,region="AREA_NAME")
-  geo_2010$year <- as.integer(2010)
-  geo_2006 <- ggplot2::fortify(shapefile_2006,region="AREA_NAME")
-  geo_2006$year <- as.integer(2006)
-  geo_2003 <- ggplot2::fortify(shapefile_2006,region="AREA_NAME") # Assuming 2006 locations for 2003 geo
-  geo_2003$year <- as.integer(2003)
-  toPollGeo <- rbind(geo_2014, geo_2010,geo_2006,geo_2003)
-  toPollGeo$ward <- as.integer(stringr::str_sub(toPollGeo$id,1,2))
-  toPollGeo$area <- as.integer(stringr::str_sub(toPollGeo$id,-3,-1))
-  toPollGeo$year <- as.factor(toPollGeo$year)
+  geo_2003 <- geo_2006 %>% # No shapefiles are available for 2003, so assuming same as 2006
+    dplyr::mutate(year = as.factor(2003))
+  rm(year, years, base_shapefile_url, shapefile_prefix)
+
+  # Merge shapefiles --------------------------------------------------------------
+
+  toPollGeo <- rbind(geo_2014, geo_2010, geo_2006, geo_2003)
   rm(geo_2014, geo_2003, geo_2006, geo_2010)
-  toPollGeo <- dplyr::inner_join(toPollGeo,toVotes::ward_regions, by=c("ward"))
+
+  # Clean up ----------------------------------------------------------------
+
+  toPollGeo <- dplyr::inner_join(toPollGeo, toVotes::ward_regions, by=c("ward"))
+  devtools::use_data(toPollGeo, overwrite = TRUE)
   toPollGeo
 }
